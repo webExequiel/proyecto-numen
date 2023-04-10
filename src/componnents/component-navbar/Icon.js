66ca1c00fbbe9c97b7b1b56515445a4623ad08f0
webExequiel <exe95@hotmail.com.ar>
2023-04-10 18:54:21 -0300

    Creamos el componente icon de fontawesome

---
+++ src/componnents/component-navbar/Icon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const Icon = (props) => {
+  return (
+
+    <span>
+        <FontAwesomeIcon icon={props} />
+    </span>
+
+  )
+}
+
+export default Icon
