9495b07c6da80f982ef862ed268cfebc0bd23f16
webExequiel <exe95@hotmail.com.ar>
2023-04-10 18:56:32 -0300

    Agregamos codigo a la funcion search

---
+++ src/componnents/component-navbar/Search.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Button from './Button';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Search = () => {
+
+  const [elementos, setElementos] = useState([]);
+  const [tablaElementos, setTablaElementos] = useState([]);
+  const [busqueda, setBusqueda] = useState('');
+
+  const peticionGet=async() => {
+    await axios.get('')
+    .then(response=>{
+      setElementos(response.data);
+      setTablaElementos(response.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+    }
+
+  const handleChange=e=>{
+      setBusqueda(e.target.value);
+      filtrar(e.target.value);
+
+    }
+
+  const filtrar = (terminoBusqueda) => {
+      var resultadosBusqueda=tablaElementos.filter((elementos) => {
+        if (elementos.nombre.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())){
+          return elementos;
+        }
+      })
+      setElementos(resultadosBusqueda);
+    }
+  return (
+
+    <div>
+
+      <Button icon={faSearch} />
+
+      <div className='containerInput'>
+        <input
+          className='form-control inputBuscar'
+          value={busqueda}
+          placeholder='busqueda por nombre'
+          onChange={handleChange}
+
+        />
+      </div>
+
+      <div className='table-responsive'>
+        <table className='table table-sm table-bordered'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOMBRE</th>
+              <th>PRECIO</th>
+              <th>DESCRIPCION</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {elementos &&
+            elementos.map((elementos) =>(
+              <tr key={elementos.id}>
+                <td>{elementos.id}</td>
+                <td>{elementos.nombre}</td>
+                <td>{elementos.precio}</td>
+                <td>{elementos.descripcion}</td>
+              </tr>
+              ))}
+          </tbody>
+
+        </table>
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Search
