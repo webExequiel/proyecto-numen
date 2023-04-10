05ca6a1be07f7474f8bf261ac569d35a2f98ca54
webExequiel <exe95@hotmail.com.ar>
2023-04-10 18:55:59 -0300

    Corregimos el componente login y funcion

---
+++ src/componnents/component-navbar/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './Button';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Login = () => {
+  return (
+
+  <Button icon={faUser} />
+
+  )
+}
+
+export default Login
