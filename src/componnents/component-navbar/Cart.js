6a2113ace420f7dc91aa6fb36487f1b52f9386d1
webExequiel <exe95@hotmail.com.ar>
2023-04-10 18:53:15 -0300

    Corregimos el componente cart y corregimos la funcion

---
+++ src/componnents/component-navbar/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+const Cart = () => {
+  return (
+
+    <Button icon={faCartShopping} />
+
+  )
+}
+
+export default Cart
