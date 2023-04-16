0ba3764c5e5b71d47699297a843add2804eefa43
mar907 <83983854+mar907@users.noreply.github.com>
2023-04-16 19:53:49 -0300

    Update src/componnents/Carrusel.js

--- src/componnents/Carrusel.js
+++ src/componnents/Carrusel.js
@@ -27,36 +27,12 @@ const options = {
   function CarruselComidas () {
     return (
       <OwlCarousel className="owl-theme" {...options}>
-        <div className="burger"><h4>1</h4>
-          <img className="imagenBurger" src= {require ("../imagenes/burger.jpg")} alt= "burger"/>
-        </div>
-        <div className="empanadas"><h4>2</h4>
-          <img className="imagenEmpanadas" src= {require ("../imagenes/empanadas.jpg")} alt= "empanadas"/>
-        </div>
-        <div className="grillados"><h4>3</h4>
-          <img className="imagenGrillados" src= {require ("../imagenes/grillados.jpg")} alt= "vegetales_grillados"/>
-        </div>
-        <div className="ensalada"><h4>4</h4>
-          <img className="imagenEnsalada" src= {require ("../imagenes/ensaladaFrutas.jpg")} alt= "ensalada_frutas"/>
-        </div>
-        <div className="barraCereal"><h4>5</h4>
-          <img className="imagenBarraCereal" src= {require ("../imagenes/barraCereal.jpg")} alt= "barra_de_cereal"/>
-        </div>
-        <div className="hamburgesa"><h4>6</h4>
-          <img className="imagenHamburgesa" src= {require ("../imagenes/hamburger.jpg")} alt= "hamburgesa"/>
-        </div>
-        <div className="Pasta"><h4>7</h4>
-          <img className="imagenPasta" src= {require ("../imagenes/pasta.jpg")} alt= "pasta_vegetal"/>
-        </div>
-        <div className="pizzaVerde"><h4>8</h4>
-          <img className="imagenPizza" src= {require ("../imagenes/pizzaVerde.jpg")} alt= "pizza_verde"/>
-        </div>
-        <div className="rollVegano"><h4>9</h4>
-          <img className="imagenRollVegano" src= {require ("../imagenes/rollVegano.jpg")} alt= "roll_vegano"/>
-        </div>
-        <div className="Jugos"><h4>10</h4>
-          <img className="imagenJugos" src= {require ("../imagenes/jugos.jpg")} alt= "jugos"/>
-        </div>
+        {images.map((image, index) => (
+            <div className={image}><h4>{index + 1}</h4>
+                <img className={`imagen${image}`} src= {require (`../imagenes/${image}.jpg`)} alt={image} />
+            </div>
+        ))}
+      
       </OwlCarousel>
     );
   }
