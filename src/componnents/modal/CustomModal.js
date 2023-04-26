400f5ae2875efd1de7e8d0e9e1bb09498e6eeda4
webExequiel <exe95@hotmail.com.ar>
2023-04-26 00:18:44 -0300

    Probando modal

--- src/componnents/modal/CustomModal.js
+++ src/componnents/modal/CustomModal.js
@@ -1,21 +1,43 @@
 
 import React from 'react'
-import { useState } from 'react';
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 const CustomModal = () => {
-    const [isOpenModal, setisOpenModal] = useState(false);
 
-    const modalOpen = () => {
-        setisOpenModal(!isOpenModal);
-    }
+  const [isOpenModal, setisOpenModal] = useState(false);
+
+  const modalOpen = () => {
+    setisOpenModal(!isOpenModal);
+  }
 
   return (
-    <div className='primary'>
-        <div className='secundary'>
-            <button color='success' onClick={modalOpen}>Mostrar boton</button>
-        </div>
-    </div>
+
+      <><div className='principal'>
+      <div className='secundario'>
+        <Button color='success'>Mostrar Modal</Button>
+      </div>
+    </div><Modal isOpen={modalOpen}>
+        <ModalHeader>
+          VIANDA
+        </ModalHeader>
+        <ModalBody>
+          VIANDA
+        </ModalBody>
+        <ModalFooter>
+          <button>AGREGAR</button>
+          <button onClick={!isOpenModal}>CERRAR</button>
+        </ModalFooter>
+      </Modal></>
+
   )
 }
 
 export default CustomModal
+
+
+
+
+
+
