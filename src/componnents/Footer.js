e3446036160c9097e7562279d314304bb45453e0
nicolas godoy <127219856+Nickgodoy87@users.noreply.github.com>
2023-05-15 17:24:59 -0300

    se modificaronestilos y se agrego redes

--- src/componnents/Footer.js
+++ src/componnents/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./footer.css";
 import styled from "styled-components";
 import Vegan from '../assets/img/logon1.png';
 import Visa from '../imagenes/visa.jpg'
@@ -9,37 +10,37 @@ import    Dinner  from '../imagenes/Diners-Club-International-logo.jpg'
 import    Shopping  from '../imagenes/shopping.png'
 import  American    from '../imagenes/american.png'
 import   MerPag   from '../imagenes/logo-mercadopago.jpg'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+ 
 
 
 
 const Body = styled.div`
-
 padding: 50px 0 0 0;
 background-color:#357401  ;
-
-
-      `
-
+`
 const IMG = styled.img`
 width:170px;
 height: 170px;
 margin-left : 35px;
-
-
 `
 const Controw = styled.div`
   display: flex ;
   flex-wrap : wrap ;
-
-
-
-`
+  margin-left : 30px;
+  `
 const Redes = styled.ul`
 display:flex ;
 `
-const Img = styled.img`
-width:60px ;
-heigth: 70px;
+const Img = styled.img `
+
+ height:50px;
+ width:110px;
 `
 const H4 = styled.h4`
 font-size :22px;
@@ -86,22 +87,15 @@ justify-content:center ;
 `
 const Listfooter =styled.div`
 text-align : center ;
-
-
 `
 const MetPag = styled.div`
 padding: 50px 0 0 0;
 display:flex;
-
-
 `
 const Tarjetas = styled.div`
-
 margin-left: 30px;
-
 `
 const Copy =styled.div`
-
 font-size :22px;
 color: #ffffff :
 text-transform:capitalize;
@@ -109,8 +103,33 @@ margin-bottom: 30px;
 font-weight: 500;
 position:relative;
 font-family: 'Hammersmith One', sans-serif;
+`
+const A2 = styled.a`
 
-
+position: relative;
+width: 60px;
+height: 60px;
+display: inline-block;
+border-radius: 10px;
+display : flex;
+justify-content: center;
+align-items: center;
+text-decoration: none;
+color: #1CA12D;
+border-color: #fff;
+font-size: 2em;
+border: 1px solid rgba(255,255,255,.4);
+border-right: 1px solid rgba (255,255,255, .2);
+border-bottom: 1px solid rgba (255,255,255, .2);
+box-shadow: 0 5px 45px rgba (0,0,0, .1);
+backdrop-filter : blur(2px);
+transition : .5s;
+overflow: hidden;
+margin-left: 15px;
+&:hover{
+  color:#ffffff;
+  background-color: #bbbbbb;
+}
 
 `
 
@@ -125,7 +144,7 @@ const Footer = () => {
         <Body>
       
       
-      <Controw >
+      <Controw className="respond" >
        
           <div className="Logo">
           <IMG src={Vegan} />
@@ -133,9 +152,9 @@ const Footer = () => {
 
         
         
-        <Contlinks>
+        <Contlinks className="footer-links">
        
-                  <Listfooter className="footer-links">
+                  <Listfooter className="">
                   <H4>Nosotros</H4>
                     <List>
                     <li><A href="">¿Quienes somos?</A></li>
@@ -157,10 +176,15 @@ const Footer = () => {
            
       
     
-        <Listfooter className="App">
+        <Listfooter>
         <H4> Sigamos conectados</H4>
-        <Redes>
-        <li><a></a></li>
+        <Redes className="Redes">
+          <A2 href=""><FontAwesomeIcon icon= {faFacebook}/></A2>
+          <A2 href=""><FontAwesomeIcon icon= {faTwitter}/></A2>
+          <A2 href=""><FontAwesomeIcon icon= {faInstagram}/></A2>
+          <A2 href=""><FontAwesomeIcon icon= {faLinkedin}/></A2>
+             
+             
             
             
           </Redes>
@@ -181,7 +205,7 @@ const Footer = () => {
         <h2 className="estilomet"><h2>Metodo de pago</h2></h2>
         </div>
         <Tarjetas className="img">
-          <Img src={Visa}></Img>
+          <Img src={Visa}/>
           <Img src={Mastercard}></Img>
           <Img src={American}></Img>
           <Img src={Naranja}></Img>
